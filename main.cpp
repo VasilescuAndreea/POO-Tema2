2d9d466412c3d31d5a956e4b4011f5e9435b6a81
Vasilescu Andreea <62394241+VasilescuAndreea@users.noreply.github.com>
2020-05-07 23:41:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ using namespace std;
 ifstream in("date.in");
 int main() {
     Agenda *arr = new Agenda();
-    Abonat_Skype_Extern *ase1 = new Abonat_Skype_Extern(1,"Miculita Marius", "0728422252", "MC_Marius", "Romania");
-    Abonat_Skype_Extern *ase2 = new Abonat_Skype_Extern(2,"Vasilescu Andreea", "0735035110", "Andreea_Vasilescu", "Romania");
+    Abonat_Skype_Extern *ase1 = new Abonat_Skype_Extern(1," Maria", "0725653448", "Maria_01", "Romania");
+    Abonat_Skype_Extern *ase2 = new Abonat_Skype_Extern(2," Andreea", "075561554", "Andreea_02", "Romania");
 
     Abonat_Skype_Romania *asr1 = new Abonat_Skype_Romania(3, "Marian ", "0766535151", "Marian_03", "marian.marian@yahoo.com");
     Abonat_Skype_Romania *asr2 = new Abonat_Skype_Romania(4, "Catalina", "0745335655", "Catalina_04", "catalina_cata@gmail.com");
